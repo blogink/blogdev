136594a2a296483160e7610e8139a5cdc9df32b7
Anonymous <anonymous@example.com>
2025-09-23 13:49:02 +0800

    Site updated: 2025-09-23 13:49:01

--- js/reurl.js
+++ js/reurl.js
@@ -1,9 +1,1 @@
-
-// 重定向浏览器地址，解决hexo-offline-popup和pjax同时使用时地址栏链接后缀的问题
-pjax.site_handleResponse = pjax.handleResponse;
-pjax.handleResponse = function(responseText, request, href, options) {
-    Object.defineProperty(request, 'responseURL', {
-        value: href
-    });
-    pjax.site_handleResponse(responseText, request, href, options);
-}
+pjax.site_handleResponse=pjax.handleResponse,pjax.handleResponse=function(e,n,s,p){Object.defineProperty(n,"responseURL",{value:s}),pjax.site_handleResponse(e,n,s,p)};
